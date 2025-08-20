64e868356e244305966fc9ba1a0ba45ead143691
AUREO\concy <concyinfo@gmail.com>
2025-08-20 08:34:58 -0300

    finalizacao da modernização

---
+++ app/src/main/java/br/com/halk/MainActivity.java
@@ -0,0 +1,31 @@
+package br.com.halk;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import br.com.hawk.Hawk;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        Hawk.init(this).build();
+
+        Hawk.put("aja","teste");
+
+        String a = Hawk.get("aja");
+
+        int x = 0;
+
+
+    }
+}
